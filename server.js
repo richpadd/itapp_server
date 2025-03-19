03ef6be2585c745fd19edaeef60db9d49d6be25b
Richard <rpadd88@gmail.com>
2025-03-19 17:25:49 +0800

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Final Project - Puzzle Checker
 // Richard Paddock (A20603128)
 // ----------------------------------------------------------------------------
-// File: server.js
+// File: app/server.js
 // Descripton: This is the node.js server main fle, it includes - 
 // - Config/startup for express to serve the front-end react app
 // - Config/startup for express to serve the back-end node.js code
@@ -43,43 +43,78 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME    
 });
 
+console.log('Connecting to',process.env.DB_NAME);
+
 // Provide an error/success message 
 db.connect(err => {
     if (err) {
         console.error('Database connection failed:', err);
         return;
     }
-    console.log('Connected to our MariaDB!');
+    console.log('Connected to our',process.env.DB_NAME,'database');
 });
 
 // --------------------------------------------------------------------------
-// API's
+// The API Section
 
-app.get('/api/test', (req, res) => {
-    // Destructure the query parameters (e.g. id)
-    const {id} = req.query;
-    
-    // Base query string
-    let query = 'SELECT * FROM test_table';
-    let conditions = [];
-    
-    // If the 'id' filter is provided, add it to the query
-    if (id) {
-        conditions.push(`id = ?`);
-    }
-      
-    // If there are any conditions, join them with 'AND' and append to the query
-    if (conditions.length > 0) {
-        query += ' WHERE ' + conditions.join(' AND ');
-    }
-    // Execute the query with the parameters (use ? placeholders for safety)
+//---------------------------------------------------------------------------
+// API - GET terms
+// Returns - category name, term name and term definition ordered by category and term
+// Return Format - JSON
+// Parameters - Optional: category_id, term_name 
+// --------------------------------------------------------------------------
 
-    console.log(query)
-    db.query(query, [id].filter(Boolean), (err, results) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
+app.get('/api/terms', (req, res) => {
+    
+    // Get any incoming query parameters - category = the category ID or name = a full or partial term name
+    const queryParams = req.query || {}; 
+
+    // Extract any passed-in parameters with default values
+    const category_id = queryParams.category_id || null;
+    const term_name = queryParams.term_name || '';
+    
+    // Set the initial SQL
+    let query = 'SELECT categories.name AS category, terms.name, terms.definition FROM categories, terms WHERE terms.category_id = categories.id' 
+    let querysort =' ORDER BY categories.id, terms.id;'
+
+    // Process any incoming parameters
+    let queryParameters = [];
+
+    // Category ID has been passed so add it to our query as a category filter, unless it's not a number in which case return an error (a safety check)
+    if (category_id) {
+        if (isNaN(category_id)){
+            return res.status(400).json({ error: 'Invalid category_id' });}
+        else {
+            query += ' AND categories.id=?'
+            queryParameters.push(category_id);
         }
-        res.json(results);
-    });
+    }
+    
+    // Term Name has been passed so add it to our query as a like so we can search on a partial match too (Our DB collation is case insenstive)
+    if (term_name) {
+        // If name is too long then return an error (a safety check)
+        if (term_name.length>100){
+            return res.status(400).json({ error: 'Invalid term name' });}
+        else {
+            query += ' AND terms.name LIKE ?'
+            queryParameters.push(`${term_name}%`);
+        }
+    }
+
+    // Add sort terms to the query
+    query += querysort;
+
+    // Output query to the log for checking
+    console.log(query)
+
+    // Execute the query using our prepared SQL with parameterised queries to avoid SQL injection and return the result
+    db.query(query, queryParameters, (err, results) => {
+    if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+    }
+    res.json(results);
+    }); 
+// --------------------------End of GET terms API
+
 });
